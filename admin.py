9126b7ef727c223c5c65a170bda6a81186b22678
Patrick Pragman <pat@pragman.io>
2021-07-11 11:19:24 -0800

    made it so you could save and recall users in the user database. -pat

--- admin.py
+++ admin.py
@@ -5,7 +5,8 @@ import passlib
 class Person:
     # Person class, contains everything you need to know
     # Default Values to give an idea of the db structure
-    def __init__(self, first_name: str = "Guy",
+    def __init__(self,
+                 first_name: str = "Guy",
                  last_name: str = "Manderson",
                  address: str = "123 E Fuckwit",
                  phone: str = "555-555-5555",
@@ -13,8 +14,10 @@ class Person:
                  username: str = "gman",
                  password: str = "unhashed_mess",
                  is_employee: bool = False,
-                 is_admin: bool = False) -> None:
-        self.id = id  # SQL id number in db
+                 is_admin: bool = False,
+                 notes: str = "Empty note.",
+                 is_active: bool = True) -> None:
+
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
@@ -25,10 +28,12 @@ class Person:
         self.is_employee = is_employee
         self.is_admin = is_admin
         self.last_logon = None
+        self.notes = notes
+        self.is_active = is_active
 
 
 class Database:
-    location = "/inventory.db"
+    location = "inventory.db"
 
     def __init__(self):
         # these dictionaries will hold data we need
@@ -39,6 +44,9 @@ class Database:
         # first let's write the SQL to update the db
         sql_query = 'select * from person'
 
+        # i repeat the following code multiple times for items and places
+        # why?  because I haven't refactored it yet - also because I'm lazy
+        # change when able
         try:
             # connect to the database
             conn = sqlite3.connect(Database.location)
@@ -51,45 +59,43 @@ class Database:
             for row in rows:
                 # Iterate through all the rodes, make a new node with the lat_lons
                 person = Person()
-                person.id = row[0]
-                person.first_name = row[1]
-                person.last_name = row[2]
-                person.address = row[3]
-                person.phone = row[4]
-                person.email = row[5]
-                person.notes = row[6]
-                person.username = row[7]
-                person.password = row[8]
-                person.is_employee = bool(row[9])
-                person.is_admin = bool(row[10])
-                person.last_logon = row[11]
+                person.first_name = row[0]
+                person.last_name = row[1]
+                person.address = row[2]
+                person.phone = row[3]
+                person.email = row[4]
+                person.notes = row[5]
+                person.username = row[6]
+                person.password = row[7]
+                person.is_employee = bool(row[8])
+                person.is_admin = bool(row[9])
+                person.last_logon = row[10]
 
                 self.people[person.username] = person
 
             conn.close() # close the database
 
         except sqlite3.Error as e:
-            print("Error loading database.  See following error:")
+            print("Error loading people.  See following error:")
             print(e)
 
-    def update_people(self) -> bool:
+    def save_people(self) -> bool:
         # first let's put all the people into the database
         # first let's write the SQL to update the db
         sql_query = """
-        update person
-        set
-            first_name = ?
-            last_name = ?
-            address = ?
-            phone = ?
-            email = ?
-            notes = ?
-            username = ?
-            password = ?
-            is_employee = ?
-            last_logon = ?
-        where
-            id = ? ;
+        insert or replace into person (
+            first_name,
+            last_name,
+            address,
+            phone,
+            email,
+            notes,
+            username,
+            password,
+            is_employee,
+            last_logon,
+            is_active)
+        values (?,?,?,?,?,?,?,?,?,?,?);
         """
 
         try:
@@ -99,7 +105,9 @@ class Database:
 
             # let's iterate through the people in the program
             # and send them to the db
-            for person in self.people:
+
+            for entry in self.people:
+                person = self.people[entry]  # the key is the var entry
                 data = (person.first_name,
                         person.last_name,
                         person.address,
@@ -110,11 +118,12 @@ class Database:
                         person.password,
                         person.is_employee,
                         person.is_admin,
-                        person.id
+                        int(person.is_active)
                         )
                 cur.execute(sql_query, data)
 
             conn.commit()
+            conn.close()
 
             result = True
 
@@ -123,5 +132,4 @@ class Database:
             print(e)
             result =  False
 
-        conn.close()  # close the database
         return result
--- inventory.py
+++ inventory.py
@@ -1,4 +1,12 @@
 # This script runs
 
+from admin import *
+
 print("hello world...")
 # test change git!
+
+db = Database()
+
+
+for name in db.people:
+    print(db.people[name].username)

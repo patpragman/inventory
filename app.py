b55136d4dc5eb2b1345ca9e6bef533d41cd416ab
Patrick Pragman <pat@pragman.io>
2021-07-13 13:51:13 -0800

    tinkering with flask stuff contemplating structure

---
+++ app.py
@@ -0,0 +1,42 @@
+import atexit
+from flask import Flask, render_template, request, redirect
+from admin import *
+
+app = Flask(__name__)
+
+# load the database into memory
+db = Database()
+# when the process dies, save everything for next time
+atexit.register(db.save_people)  # save people
+atexit.register(db.save_items)  # save items
+atexit.register(db.save_places)  # save people
+
+
+@app.route("/")
+def main() -> str:
+
+    return "Main page. Goes here."
+
+
+@app.route("/login")
+def login() -> str:
+
+    return "Login goes here."
+
+
+@app.route("/user_list")
+def user_list() -> str:
+    # this is the start of
+    output = str([person for person in db.people])
+    return output
+
+
+@app.route("/new_user")
+def new_user() -> str:
+
+    return "New user."
+
+
+# we'll use SSL to hide submitted passwords
+if __name__ == '__main__':
+    app.run(ssl_context="adhoc")
